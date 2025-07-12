eb6a89fb1f03ca7279f52b57aa9e5889788a0b1d
hoilc <hoilc@foxmail.com>
2025-07-13 05:39:57 +0800

    test3

---
+++ bin/patchTestOutput.ps1
@@ -0,0 +1,23 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$filePath
+)
+
+$insertCode = @'
+if ($env:BHBuildSystem -eq 'GitHub Actions') {
+    $splat.OutputFile = "$env:GITHUB_WORKSPACE\TestResults.xml"
+    $splat.OutputFormat = 'NUnitXML'
+}
+'@
+
+if (-not (Test-Path $filePath)) {
+    Write-Error "File not found: $filePath"
+    exit 1
+}
+
+(Get-Content $filePath) | ForEach-Object {
+    $_
+    if ($_ -match '# GitHub Actions') {
+        $insertCode
+    }
+} | Set-Content $filePath -Encoding UTF8
