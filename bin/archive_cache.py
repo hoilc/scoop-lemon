41613726b84a61b641990db33862041af6ebf442
hoilc <hoilc@foxmail.com>
2026-02-03 01:45:50 +0800

    chore: slow down

--- bin/archive_cache.py
+++ bin/archive_cache.py
@@ -141,7 +141,7 @@ def process_cache_files():
     download_suffix = '.download'
 
     session = get_session()
-    adapter = RateLimitedAdapter(max_calls=5, period=30)
+    adapter = RateLimitedAdapter(max_calls=3, period=20)
     session.mount('https://', adapter)
     session.mount('http://', adapter)
 
