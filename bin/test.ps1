6b29e630f08e58622725976cd2ce2b366b91abe1
hoilc <hoilc@foxmail.com>
2026-08-13 02:02:25 +0800

    chore: Remove unused files

--- bin/test.ps1
+++
@@ -1,8 +0,0 @@
-<#
-.SYNOPSIS
-    Execute Pester tests in repository root directory.
-#>
-
-$result = Invoke-Pester "$PSScriptRoot\..\Scoop-Bucket.Tests.Local.ps1" -PassThru
-
-exit $result.FailedCount
